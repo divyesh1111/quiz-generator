2c2d5c950bfa364f702816630641fa6f9d365b24
divyesh1111 <divyeshhirpara000@gmail.com>
2026-03-27 13:45:09 -0700

    feat: register quiz services and update imports for models and services

--- QuizGenerator/Program.cs
+++ QuizGenerator/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QuizGenerator;
+using QuizGenerator.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+
+// Register Quiz Services
+builder.Services.AddSingleton<IQuestionBankService, QuestionBankService>();
+builder.Services.AddSingleton<IQuizService, QuizService>();
 
 await builder.Build().RunAsync();
